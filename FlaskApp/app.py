daee7d367a5c47ba2b6b255779a552fafb2bc02f
minhptruong <mt3077@columbia.edu>
2016-10-22 23:59:02 -0400

    test locations

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template
 app=Flask(__name__)
 
+mylocation = []
+mylocation.append(40.741895)
+mylocation.append(-73.989308)
+
 @app.route("/")
 def main():
-	return render_template('index.html')
-
+	return render_template('index.html',context=mylocation)
 
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(port=8000,debug=True)
