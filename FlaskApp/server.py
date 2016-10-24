41bc50859c718197a6ea4d71e8d77546977e9895
agango <ag3661@columbia.edu>
2016-10-24 03:55:47 -0400

    fucking server is finally working

--- FlaskApp/server.py
+++ FlaskApp/server.py
@@ -9,11 +9,12 @@ access_token="790035594604867584-Jhmohj3Dk2z5LotbHwzm9LFFLFgfUjc"
 access_token_secret="XVXmduRvXAr3z3BSXfEPQzwyisrbwAcIuktCLYWcywjM2"
 consumer_key="XlLab1tBNL1NAws6FwAjL8r3i"
 consumer_secret="bzcuCYinbhJ63KfbD7e7I8pdE9ZWCvRx58X2Dm7cFCQFeTPFZP"
+
 es=Elasticsearch("https://search-newtweetmapcloud-cqcpw6xnfeq2dnscys2soadpe4.us-west-2.es.amazonaws.com")
-searchArray=['embiid', 'westbrook', 'durant', 'curry', 'draymond']
 class StdOutListener(StreamListener):
     
     def on_data(self, doc_data):
+        print doc_data
         json_data=json.loads(doc_data)
         try:
             data={}
@@ -29,7 +30,7 @@ class StdOutListener(StreamListener):
             data["name"]=json_data['user']['name']
             data["created_at"]=json_data['created_at']
             print data
-            res=es.index(index="new_cloud", doc_type='tweet', body=data)
+            res=es.index(index="cloud", doc_type='tweet', body=data)
             print res['created']
         except:
             return 
@@ -44,7 +45,7 @@ if __name__ == "__main__":
             auth = OAuthHandler(consumer_key, consumer_secret)
             auth.set_access_token(access_token, access_token_secret)
             stream = Stream(auth, l)
-            stream.filter(track=searchArray, locations=[-180, -90, 180, 90])
+            stream.filter(track=['car', 'house', 'country'])
 
         except:
             continue
