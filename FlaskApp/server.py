8edb6569a2aa53b549998b0f4155d475f9ae350b
agango <ag3661@columbia.edu>
2016-10-29 00:06:16 -0400

    changing to localhost elasticsearch instance for bonus

--- FlaskApp/server.py
+++ FlaskApp/server.py
@@ -9,29 +9,49 @@ access_token="790035594604867584-Jhmohj3Dk2z5LotbHwzm9LFFLFgfUjc"
 access_token_secret="XVXmduRvXAr3z3BSXfEPQzwyisrbwAcIuktCLYWcywjM2"
 consumer_key="XlLab1tBNL1NAws6FwAjL8r3i"
 consumer_secret="bzcuCYinbhJ63KfbD7e7I8pdE9ZWCvRx58X2Dm7cFCQFeTPFZP"
-
-es=Elasticsearch("https://search-newtweetmapcloud-cqcpw6xnfeq2dnscys2soadpe4.us-west-2.es.amazonaws.com")
+#mapping='''
+#    {
+#        "mappings":{
+#            "marker":{
+#                "properties":{
+#                    "location":{
+#                        "type":"geo_point",
+##                        "lat_lon":true
+ #                   },
+ #                   "name":{
+ #                       "type":"string"
+ ##                   },
+  #                  "text":{
+  #                      "type":"string"
+  ##                  }
+ #               }
+ #           }
+ #       }
+ #   }'''
+es=Elasticsearch()
+#es.indices.create(index="newtweetcloudfinal", body=mapping)
 class StdOutListener(StreamListener):
     
     def on_data(self, doc_data):
-        print doc_data
         json_data=json.loads(doc_data)
         try:
             data={}
             if json_data['coordinates'] is not None:
-                data["lat"]=json_data['coordinates']['coordinates'][0]
-                data["longtitude"]=longtitude=json_data['coordinates']['coordinates'][1]
+                data["location"]={}
+                data["location"]["type"]="geo_point"
+                data["location"]["lat"]=json_data['coordinates']['coordinates'][0]
+                data["location"]["lon"]=json_data['coordinates']['coordinates'][1]
             elif json_data['place'] is not None:
-                data["lat"]=json_data['place']['bounding_box']['coordinates'][0][0][0]
-                data["longtitude"]=json_data['place']['bounding_box']['coordinates'][0][0][1]
+                data["location"]=={}
+                data["location"]["type"]="geo_point"
+                data["location"]["lat"]=json_data['place']['bounding_box']['coordinates'][0][0][0]
+                data["location"]["lon"]=json_data['place']['bounding_box']['coordinates'][0][0][1]
             else:
                 return
             data["text"]=json_data['text']
             data["name"]=json_data['user']['name']
-            data["created_at"]=json_data['created_at']
-            res=es.index(index="cloud", doc_type='tweet', body=data)
-            print data
-            print res['created']
+            res=es.index(index="newtweetcloudfinal", doc_type='tweet', body=data)
+            print (res['created'])
         except:
             return 
 
